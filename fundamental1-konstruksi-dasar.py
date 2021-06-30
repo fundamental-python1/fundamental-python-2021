a4a1e32110d0918ba965b192bb7cf587d8be2420
antenavv <86529016+antenavv@users.noreply.github.com>
2021-07-01 05:59:12 +0800

    selesai fundamental python

---
+++ fundamental1-konstruksi-dasar.py
@@ -0,0 +1,18 @@
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL : Eksekusi Berurutan
+print('Hello World!')
+print('By Antenav')
+print('Tanggal 1 Juli 2021')
+print('-' * 10)
+
+# PERCABANGAN : Eksekusi terpilih
+ingin_cepat = False
+if ingin_cepat:
+    print('jalan lurus aja ya!')
+else:
+    print('jalan lain')
+
+# PERULANGAN
+jumlah_anak = 4
+for index_anak in range(1,jumlah_anak+1):
+    print(f'Halo anak #{index_anak}')
