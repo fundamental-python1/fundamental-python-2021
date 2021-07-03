98957155dc12655cfd24289bdc3fd54abc06e20e
antenav <antenavmail@gmail.com>
2021-07-03 21:42:04 +0800

    selesai fundamental python

--- fundamental2-tipe-data-dict.py
+++ fundamental2-tipe-data-dict.py
@@ -10,12 +10,17 @@ print(kamus_ind_eng)
 print(kamus_ind_eng['ayah'])
 print(kamus_ind_eng['ibu'])
 
-print('Data ini dikirimkan oleh server Gojek, untuk memberikan info driver disekitar pemakai aplikasi')
+print('\nData ini dikirimkan oleh server Gojek, untuk memberikan info driver disekitar pemakai aplikasi')
 data_dari_server_gojek = {
     'tanggal': '2021-07-03',
-    'driver_list': [{'nama': 'Eka', 'jarak':10}, {'nama': 'Dwi', 'jarak': '30'}, {'[nama': 'Tri', 'jarak': '100'}, {'nama': 'Catur','jarak': '1000'}]
-
+    'driver_list': [
+        {'nama': 'Eka', 'jarak': 10},
+        {'nama': 'Dwi', 'jarak': '30'},
+        {'[nama': 'Tri', 'jarak': '100'},
+        {'nama': 'Catur', 'jarak': '1000'}]
+}
 print(data_dari_server_gojek)
-print(f"Driver disekitar sini {data_dari_server_gojek['driver_list']}")
+print(f"\nDriver disekitar sini {data_dari_server_gojek['driver_list']}")
 print(f"Driver #1 {data_dari_server_gojek['driver_list'][0]}")
 print(f"Driver #4 {data_dari_server_gojek['driver_list'][3]}")
+print (f"Driver terdekat berjarak {data_dari_server_gojek['driver_list'][0]['jarak']} meter")
